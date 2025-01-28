39fcd75d1c1e972ecb3f79f612f4a66f0628c31a
Cestercian <yashafaid@gmail.com>
2025-01-28 15:07:32 +0530

    V.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import Die from "./Die.jsx";
+
+import Main from "./Main.jsx";
 
 function App() {
   return (
-    <>
-        <main>
-            <div className={"dice-container"}>
-                <Die
-                    number={5}
-                />
-                <Die
-                    number={1}
-                />
-                <Die
-                    number={6}
-                />
-                <Die
-                    number={7}
-                />
-                <Die
-                    number={1}
-                />
-                <Die
-                    number={9}
-                />
-                <Die
-                    number={3}
-                />
-                <Die
-                    number={4}
-                />
-                <Die
-                    number={1}
-                />
-                <Die
-                    number={2}
-                />
-            </div>
-        </main>
-    </>
+    <Main/>
   )
 }
 
