7633aaeb53a0dfac652930f7087bc89c1cabe630
Cestercian <yashafaid@gmail.com>
2025-01-28 18:14:54 +0530

    V.1.1

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,11 @@
-import {useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Die from "./Die.jsx";
+import { nanoid } from "nanoid";
 
-export default function (){
+export default function () {
+  const [numbers, setNumbers] = useState([]);
 
-    const [number,setNumbers]=useState([])
-
-    let [count,setCount] = useState(0)
+  let [count, setCount] = useState(0);
 
     function inCount(){
         setCount((prevState)=>
@@ -13,36 +13,34 @@ export default function (){
         )
     }
 
-    useEffect(()=>{
-        let numbersArr = []
-        function generateNO(){
-            for(let i=0;i<10;i++){
-                const randomNo = Math.ceil(Math.random()*6)
-                numbersArr.push(randomNo)
-            }
-            setNumbers(numbersArr)
-        }
-        generateNO()
-    },[count])
+  useEffect(() => {
+    let numbersArr = [];
+    function generateNO() {
+      for (let i = 0; i < 10; i++) {
+        const randomNo = Math.ceil(Math.random() * 6);
+        numbersArr.push({
+          value: randomNo,
+          isHeld: false,
+          id: nanoid(),
+        });
+      }
+      setNumbers(numbersArr);
+    }
+    generateNO();
+  }, [count]);
 
-    const diceElements = number.map((num)=>
-        <Die key={index} number={num}/>
-    )
+  const diceElements = numbers.map((num) => (
+    <Die number={num.value} key={num.id} />
+  ));
 
-    return(
-        <>
-            <main>
-                <p></p>
-                <p></p>
-                <div className={"dice-container"}>
-                    {diceElements}
-                </div>
-                <p></p>
-                <p></p>
-                <button onClick={inCount}>
-                    Roll Dice
-                </button>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main>
+        <div className={"dice-container"}>{diceElements}</div>
+        <button className={"roll-dice"} onClick={inCount}>
+          Roll Dice
+        </button>
+      </main>
+    </>
+  );
 }
