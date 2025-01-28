ddd0847aa17a0eb6ab3bd564170617f8ce73cabc
Cestercian <yashafaid@gmail.com>
2025-01-28 17:35:09 +0530

    V.1.1

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,6 +5,14 @@ export default function (){
 
     const [number,setNumbers]=useState([])
 
+    let [count,setCount] = useState(0)
+
+    function inCount(){
+        setCount((prevState)=>
+            prevState+1
+        )
+    }
+
     useEffect(()=>{
         let numbersArr = []
         function generateNO(){
@@ -15,18 +23,25 @@ export default function (){
             setNumbers(numbersArr)
         }
         generateNO()
-    },[])
+    },[count])
 
     const diceElements = number.map((num)=>
-        <Die number={num}/>
+        <Die key={index} number={num}/>
     )
 
     return(
         <>
             <main>
+                <p></p>
+                <p></p>
                 <div className={"dice-container"}>
                     {diceElements}
                 </div>
+                <p></p>
+                <p></p>
+                <button onClick={inCount}>
+                    Roll Dice
+                </button>
             </main>
         </>
     )
