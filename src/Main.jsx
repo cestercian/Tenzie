92d30b59adf4d8b2642e30ef3791f9ca8b4c175b
Cestercian <yashafaid@gmail.com>
2025-01-30 02:29:18 +0530

    V.2.0

--- src/Main.jsx
+++ src/Main.jsx
@@ -69,6 +69,9 @@ export default function () {
   return (
     <>
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same.
+          Click each die to freeze it at its current value between rolls.</p>
         <div className={"dice-container"}>{diceElements}</div>
           <p></p><p></p>
         <button className={"roll-dice"} onClick={inCount}>
